a73f39ebec6312fc280cf21be2012149aaacafd1
yue <yue.dev@pm.me>
2026-03-18 16:22:51 -0600

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://nicrosil.dev",
+
   vite: {
     plugins: [tailwindcss()]
   },
